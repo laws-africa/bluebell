2366ab86b55c76718fb423d5fd7b903e7f04261d
goose-life <32566441+goose-life@users.noreply.github.com>
2020-09-04 20:15:50 +0200

    test

--- tests/test_eids.py
+++ tests/test_eids.py
@@ -1,58 +1,80 @@
 from unittest import TestCase
 
-from bluebell.xml import IdGenerator
+from lxml import etree
+from .support import ParserSupport
 
 
-class IdGeneratorTestCase(TestCase):
-    generator = IdGenerator()
+class IdGeneratorTestCase(ParserSupport, TestCase):
+    maxDiff = None
 
     def test_clean_num(self):
         self.assertEqual(
             "",
-            self.generator.clean_num(""),
+            self.generator.ids.clean_num(""),
         )
 
         self.assertEqual(
             "",
-            self.generator.clean_num(" "),
+            self.generator.ids.clean_num(" "),
         )
 
         self.assertEqual(
             "",
-            self.generator.clean_num("( )"),
+            self.generator.ids.clean_num("( )"),
         )
 
         self.assertEqual(
             "6",
-            self.generator.clean_num("(6)"),
+            self.generator.ids.clean_num("(6)"),
         )
 
         self.assertEqual(
             "16",
-            self.generator.clean_num("[16]"),
+            self.generator.ids.clean_num("[16]"),
         )
 
         self.assertEqual(
             "123.4-5",
-            self.generator.clean_num("(123.4-5)"),
+            self.generator.ids.clean_num("(123.4-5)"),
         )
 
         self.assertEqual(
             "12",
-            self.generator.clean_num("(12)"),
+            self.generator.ids.clean_num("(12)"),
         )
 
         self.assertEqual(
             "312.32.7",
-            self.generator.clean_num("312.32.7"),
+            self.generator.ids.clean_num("312.32.7"),
         )
 
         self.assertEqual(
             "312-32-7",
-            self.generator.clean_num("312-32-7"),
+            self.generator.ids.clean_num("312-32-7"),
         )
 
         self.assertEqual(
             "312_32_7",
-            self.generator.clean_num("312_32_7"),
+            self.generator.ids.clean_num("312_32_7"),
         )
+
+    def test_hier_element_heading(self):
+        tree = self.parse("""
+PARA Any-1.ing)([but-a-s=ace - Now we're in the heading!
+    Para text.
+""", 'doc')
+
+        xml = etree.tostring(self.to_xml(tree.to_dict()), encoding='unicode', pretty_print=True)
+
+        self.assertEqual("""<doc xmlns="http://docs.oasis-open.org/legaldocml/ns/akn/3.0">
+  <mainBody>
+    <paragraph eId="para_Any-1.ingbut-a-s=ace">
+      <num>Any-1.ing)([but-a-s=ace</num>
+      <heading>Now we're in the heading!</heading>
+      <content>
+        <p eId="para_Any-1.ingbut-a-s=ace__p_1">Para text.</p>
+      </content>
+    </paragraph>
+  </mainBody>
+</doc>
+""", xml)
