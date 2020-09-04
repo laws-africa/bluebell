6288c41a524dd45f9d6d31713d7b79c029e1ad39
goose-life <32566441+goose-life@users.noreply.github.com>
2020-09-04 16:44:42 +0200

    add test_eids_no_num, update others that use part

--- tests/test_eids.py
+++ tests/test_eids.py
@@ -1,9 +1,11 @@
 from unittest import TestCase
 
+from lxml import etree
 from .support import ParserSupport
 
 
 class IdGeneratorTestCase(ParserSupport, TestCase):
+    maxDiff = None
 
     def test_clean_num(self):
         self.assertEqual(
@@ -55,3 +57,57 @@ class IdGeneratorTestCase(ParserSupport, TestCase):
             "312_32_7",
             self.generator.ids.clean_num("312_32_7"),
         )
+
+    def test_eids_no_num(self):
+        tree = self.parse("""
+PARA
+    Intro
+
+PARA 1.
+    First para
+
+PARA 1A.
+    Added in later
+
+PARA
+    Unnumbered
+
+PARA 2.
+    Second (actually third/fourth/fifth, depending on who's counting) para.
+""", 'doc')
+
+        xml = etree.tostring(self.to_xml(tree.to_dict()), encoding='unicode', pretty_print=True)
+
+        self.assertEqual("""<doc xmlns="http://docs.oasis-open.org/legaldocml/ns/akn/3.0">
+  <mainBody>
+    <paragraph eId="para_nn-1">
+      <content>
+        <p eId="para_nn-1__p_1">Intro</p>
+      </content>
+    </paragraph>
+    <paragraph eId="para_1">
+      <num>1.</num>
+      <content>
+        <p eId="para_1__p_1">First para</p>
+      </content>
+    </paragraph>
+    <paragraph eId="para_1A">
+      <num>1A.</num>
+      <content>
+        <p eId="para_1A__p_1">Added in later</p>
+      </content>
+    </paragraph>
+    <paragraph eId="para_nn-2">
+      <content>
+        <p eId="para_nn-2__p_1">Unnumbered</p>
+      </content>
+    </paragraph>
+    <paragraph eId="para_2">
+      <num>2.</num>
+      <content>
+        <p eId="para_2__p_1">Second (actually third/fourth/fifth, depending on who's counting) para.</p>
+      </content>
+    </paragraph>
+  </mainBody>
+</doc>
+""", xml)
