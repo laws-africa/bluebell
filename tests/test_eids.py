b557a901fa41dea1edd1e1017e391a4e5ba53a1c
goose-life <32566441+goose-life@users.noreply.github.com>
2020-09-04 15:20:27 +0200

    refactor test_eids

--- tests/test_eids.py
+++ tests/test_eids.py
@@ -1,58 +1,57 @@
 from unittest import TestCase
 
-from bluebell.xml import IdGenerator
+from .support import ParserSupport
 
 
-class IdGeneratorTestCase(TestCase):
-    generator = IdGenerator()
+class IdGeneratorTestCase(ParserSupport, TestCase):
 
     def test_clean_num(self):
         self.assertEqual(
             "",
-            self.generator.clean_num(""),
+            self.generator.ids.clean_num(""),
         )
 
         self.assertEqual(
             "",
-            self.generator.clean_num(" "),
+            self.generator.ids.clean_num(" "),
         )
 
         self.assertEqual(
             "",
-            self.generator.clean_num("( )"),
+            self.generator.ids.clean_num("( )"),
         )
 
         self.assertEqual(
             "6",
-            self.generator.clean_num("(6)"),
+            self.generator.ids.clean_num("(6)"),
         )
 
         self.assertEqual(
             "16",
-            self.generator.clean_num("[16]"),
+            self.generator.ids.clean_num("[16]"),
         )
 
         self.assertEqual(
             "123.4-5",
-            self.generator.clean_num("(123.4-5)"),
+            self.generator.ids.clean_num("(123.4-5)"),
         )
 
         self.assertEqual(
             "12",
-            self.generator.clean_num("(12)"),
+            self.generator.ids.clean_num("(12)"),
         )
 
         self.assertEqual(
             "312.32.7",
-            self.generator.clean_num("312.32.7"),
+            self.generator.ids.clean_num("312.32.7"),
         )
 
         self.assertEqual(
             "312-32-7",
-            self.generator.clean_num("312-32-7"),
+            self.generator.ids.clean_num("312-32-7"),
         )
 
         self.assertEqual(
             "312_32_7",
-            self.generator.clean_num("312_32_7"),
+            self.generator.ids.clean_num("312_32_7"),
         )
