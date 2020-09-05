dc6701180feea404dd24af50b646e09f92d7e48c
goose-life <32566441+goose-life@users.noreply.github.com>
2020-09-05 17:15:02 +0200

    more tests

--- tests/test_eids.py
+++ tests/test_eids.py
@@ -78,3 +78,79 @@ PARA Any-1.ing)([but-a-s=ace - Now we're in the heading!
   </mainBody>
 </doc>
 """, xml)
+
+        tree = self.parse("""
+    PARA 2.3-4.5. - Now we're in the heading!
+        Para text.
+    """, 'doc')
+
+        xml = etree.tostring(self.to_xml(tree.to_dict()), encoding='unicode', pretty_print=True)
+
+        self.assertEqual("""<doc xmlns="http://docs.oasis-open.org/legaldocml/ns/akn/3.0">
+  <mainBody>
+    <paragraph eId="para_2.3-4.5">
+      <num>2.3-4.5.</num>
+      <heading>Now we're in the heading!</heading>
+      <content>
+        <p eId="para_2.3-4.5__p_1">Para text.</p>
+      </content>
+    </paragraph>
+  </mainBody>
+</doc>
+""", xml)
+
+    def test_hier_element_num_only(self):
+        tree = self.parse("""
+PARA 2.3-4.5.
+    Para text.
+""", 'doc')
+
+        xml = etree.tostring(self.to_xml(tree.to_dict()), encoding='unicode', pretty_print=True)
+
+        self.assertEqual("""<doc xmlns="http://docs.oasis-open.org/legaldocml/ns/akn/3.0">
+  <mainBody>
+    <paragraph eId="para_2.3-4.5">
+      <num>2.3-4.5.</num>
+      <content>
+        <p eId="para_2.3-4.5__p_1">Para text.</p>
+      </content>
+    </paragraph>
+  </mainBody>
+</doc>
+""", xml)
+
+    def test_hier_element_heading_only(self):
+        tree = self.parse("""
+    PARA - Now we're in the heading!
+        Para text.
+    """, 'doc')
+
+        xml = etree.tostring(self.to_xml(tree.to_dict()), encoding='unicode', pretty_print=True)
+
+        self.assertEqual("""<doc xmlns="http://docs.oasis-open.org/legaldocml/ns/akn/3.0">
+  <mainBody>
+    <paragraph eId="para_nn-1">
+      <heading>Now we're in the heading!</heading>
+      <content>
+        <p eId="para_nn-1__p_1">Para text.</p>
+      </content>
+    </paragraph>
+  </mainBody>
+</doc>
+""", xml)
+
+    def test_not_hier_element_heading(self):
+        tree = self.parse("""
+PARA Literally anything but a - Now we're in the heading!
+    Para text.
+""", 'doc')
+
+        xml = etree.tostring(self.to_xml(tree.to_dict()), encoding='unicode', pretty_print=True)
+
+        self.assertEqual("""<doc xmlns="http://docs.oasis-open.org/legaldocml/ns/akn/3.0">
+  <mainBody>
+    <p eId="p_1">PARA Literally anything but a - Now we're in the heading!</p>
+    <p eId="p_2">Para text.</p>
+  </mainBody>
+</doc>
+""", xml)
