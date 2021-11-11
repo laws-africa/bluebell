f2f38290ac004f013d2c5a58ff67349fa4a079e6
goose-life <32566441+goose-life@users.noreply.github.com>
2021-11-11 13:18:09 +0200

    add optional attachment/s inside attachments (FAILING)

--- tests/test_attachments.py
+++ tests/test_attachments.py
@@ -346,3 +346,129 @@ schedule text
   </attachment>
 </attachments>
 """, xml)
+
+    def test_nested_attachments(self):
+        tree = self.parse("""
+ANNEXURE a heading
+  SUBHEADING subheading
+
+  some text
+
+  SCHEDULE heading
+
+    schedule text
+""", 'attachment')
+        self.assertEqual({
+            'type': 'element',
+            'name': 'attachment',
+            'attribs': {'contains': 'originalVersion', 'name': 'annexure'},
+            'heading': [{
+                'type': 'text',
+                'value': 'a heading'}],
+            'subheading': [{
+                'type': 'text',
+                'value': 'subheading'}],
+            'children': [{
+                'type': 'content',
+                'name': 'p',
+                'children': [{
+                    'type': 'text',
+                    'value': 'some text'}]
+            }, {
+                'type': 'element',
+                'name': 'attachment',
+                'attribs': {'contains': 'originalVersion', 'name': 'schedule'},
+                'heading': [{
+                    'type': 'text',
+                    'value': 'heading',
+                }],
+                'children': [{
+                    'name': 'p',
+                    'type': 'content',
+                    'children': [{
+                        'type': 'text',
+                        'value': 'some text',
+                    }],
+                }],
+            }]
+        }, tree.to_dict())
+
+        xml = etree.tostring(self.to_xml(tree.to_dict()), encoding='unicode', pretty_print=True)
+        today = datestring(date.today())
+
+        self.assertEqual(f"""<attachment xmlns="http://docs.oasis-open.org/legaldocml/ns/akn/3.0" eId="att_1">
+  <heading>a heading</heading>
+  <subheading>subheading</subheading>
+  <doc contains="originalVersion" name="annexure">
+    <meta>
+      <identification source="#cobalt">
+        <FRBRWork>
+          <FRBRthis value="/akn/za/act/2009/10/!annexure_1"/>
+          <FRBRuri value="/akn/za/act/2009/10"/>
+          <FRBRalias value="Untitled" name="title"/>
+          <FRBRdate date="2009" name="Generation"/>
+          <FRBRauthor href=""/>
+          <FRBRcountry value="za"/>
+          <FRBRnumber value="10"/>
+        </FRBRWork>
+        <FRBRExpression>
+          <FRBRthis value="/akn/za/act/2009/10/eng/!annexure_1"/>
+          <FRBRuri value="/akn/za/act/2009/10/eng"/>
+          <FRBRdate date="{today}" name="Generation"/>
+          <FRBRauthor href=""/>
+          <FRBRlanguage language="eng"/>
+        </FRBRExpression>
+        <FRBRManifestation>
+          <FRBRthis value="/akn/za/act/2009/10/eng/!annexure_1"/>
+          <FRBRuri value="/akn/za/act/2009/10/eng"/>
+          <FRBRdate date="{today}" name="Generation"/>
+          <FRBRauthor href=""/>
+        </FRBRManifestation>
+      </identification>
+      <references source="#cobalt">
+        <TLCOrganization eId="cobalt" href="https://github.com/laws-africa/cobalt" showAs="cobalt"/>
+      </references>
+    </meta>
+    <mainBody>
+      <p eId="att_1__p_1">some text</p>
+    </mainBody>
+    <attachment eId="att_1__att_1">
+      <heading>heading</heading>
+      <doc contains="originalVersion" name="schedule">
+        <meta>
+          <identification source="#cobalt">
+            <FRBRWork>
+              <FRBRthis value="/akn/za/act/2009/10/!annexure_1__schedule_1"/>
+              <FRBRuri value="/akn/za/act/2009/10"/>
+              <FRBRalias value="Untitled" name="title"/>
+              <FRBRdate date="2009" name="Generation"/>
+              <FRBRauthor href=""/>
+              <FRBRcountry value="za"/>
+              <FRBRnumber value="10"/>
+            </FRBRWork>
+            <FRBRExpression>
+              <FRBRthis value="/akn/za/act/2009/10/eng/!annexure_1__schedule_1"/>
+              <FRBRuri value="/akn/za/act/2009/10/eng"/>
+              <FRBRdate date="{today}" name="Generation"/>
+              <FRBRauthor href=""/>
+              <FRBRlanguage language="eng"/>
+            </FRBRExpression>
+            <FRBRManifestation>
+              <FRBRthis value="/akn/za/act/2009/10/eng/!annexure_1__schedule_1"/>
+              <FRBRuri value="/akn/za/act/2009/10/eng"/>
+              <FRBRdate date="{today}" name="Generation"/>
+              <FRBRauthor href=""/>
+            </FRBRManifestation>
+          </identification>
+          <references source="#cobalt">
+            <TLCOrganization eId="cobalt" href="https://github.com/laws-africa/cobalt" showAs="cobalt"/>
+          </references>
+        </meta>
+        <mainBody>
+          <p eId="att_1__att_1__p_1">schedule text</p>
+        </mainBody>
+      </doc>
+    </attachment>
+    </doc>
+</attachment>
+""", xml)
